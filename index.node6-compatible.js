9ceda950585a7dbdbbf29ea3d52c67fb62acab52
rodriguj-alexva <jmr@ieee.org>
2020-05-11 09:30:22 -0700

    Update index.node6-compatible.js

--- index.node6-compatible.js
+++ index.node6-compatible.js
@@ -64,7 +64,7 @@ class MergeIntoFile {
       callback();
       return;
     }
-    Console.log(this.options);
+    console.log(this.options);
     
     const generatedFiles = {};
     let filesCanonical = [];
